0e759da2f773d2c8c8d1d716040c8dc8159147a7
ArturLaski <162509395+ArturLaski@users.noreply.github.com>
2025-02-10 23:21:16 +0100

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Description, Options, Feedback, Notification } from '@components';
+
+const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('saved-feedback');
+    if (!savedFeedback) {
+      return {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      };
+    }
+
+    return JSON.parse(savedFeedback);
+  });
+
+  useEffect(() => {
+    localStorage.setItem('saved-feedback', JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalFeedback = feedback.bad + feedback.neutral + feedback.good;
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main>
+      <Description />
+      <Options
+        options={feedback}
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        total={totalFeedback}
+      />
 
-export default App
+      {totalFeedback === 0 ? (
+        <Notification />
+      ) : (
+        <Feedback
+          options={feedback}
+          totalPoints={totalFeedback}
+          positivePoints={positiveFeedback}
+        />
+      )}
+    </main>
+  );
+};
+export default App;
